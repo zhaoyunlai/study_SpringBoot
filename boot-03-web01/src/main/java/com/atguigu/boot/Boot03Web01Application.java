1368e39acff02bc03c0440b756b06ab7f9c7087c
zhaoyunlai <zylai0712@163.com>
2022-08-07 15:18:30 +0800

    SpringBoot 处理静态资源

---
+++ boot-03-web01/src/main/java/com/atguigu/boot/Boot03Web01Application.java
@@ -0,0 +1,13 @@
+package com.atguigu.boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Boot03Web01Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Boot03Web01Application.class, args);
+    }
+
+}
---
+++ boot-03-web01/src/main/java/com/atguigu/boot/controller/HelloController.java
@@ -0,0 +1,18 @@
+package com.atguigu.boot.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @Author: Zhao YunLai
+ * @Date: 2022/08/06/17:58
+ * @Description:
+ */
+@RestController
+public class HelloController {
+
+//    @RequestMapping("/makabaka.jpg")
+//    public String hello(){
+//        return "动态请求";
+//    }
+}
