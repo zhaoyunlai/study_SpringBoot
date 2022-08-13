1cacedce6a67dab74ec8e69b8c22216d9527ddc3
zhaoyunlai <zylai0712@163.com>
2022-08-13 10:54:35 +0800

    SpringBoot CURD案例

--- boot-04-web-admin/src/main/java/com/atguigu/boot/config/MyBatisConfig.java
+++ boot-04-web-admin/src/main/java/com/atguigu/boot/config/MyBatisConfig.java
@@ -20,13 +20,21 @@ public class MyBatisConfig {
 
     /**
      * 新的分页插件,一缓和二缓遵循mybatis的规则,
-     * 需要设置 MybatisConfiguration#useDeprecatedExecutor = false 避免缓存出现问题(该属性会在旧插件移除后一同移除)
+     * 需要设置 MybatisConfiguration#useDeprecatedExecutor = false
+     * 避免缓存出现问题(该属性会在旧插件移除后一同移除)
+     * 最新版(3.5.1)没有这个属性，不用设置
      */
     // 最新版
     @Bean
     public MybatisPlusInterceptor mybatisPlusInterceptor() {
         MybatisPlusInterceptor interceptor = new MybatisPlusInterceptor();
-        interceptor.addInnerInterceptor(new PaginationInnerInterceptor(DbType.MYSQL));
+        //对于只有一种类型的数据库，
+        // 创建PaginationInnerInterceptor对象的时候最好指定数据库类型
+        PaginationInnerInterceptor paginationInnerInterceptor = new PaginationInnerInterceptor(DbType.MYSQL);
+        //可以指定一些其他的属性，一般使用默认的就行
+        //设置单页分页最大条数（默认不限制）
+        paginationInnerInterceptor.setMaxLimit(500L);
+        interceptor.addInnerInterceptor(paginationInnerInterceptor);
         return interceptor;
     }
 
--- boot-04-web-admin/src/main/java/com/atguigu/boot/controller/TableController.java
+++ boot-04-web-admin/src/main/java/com/atguigu/boot/controller/TableController.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
@@ -40,6 +41,17 @@ public class TableController {
 
         model.addAttribute("page",userPage);
         return "table/dynamic_table";
+    }
+
+    //为了方便，就直接用get了
+    @GetMapping("/user/delete/{id}")
+    public String deleteUser(@PathVariable("id") Long id,
+                             @RequestParam(value = "current",defaultValue = "1") Integer current){
+
+        userService.removeById(id);
+        System.out.println(id);
+
+        return "redirect:/dynamic_table?current="+current;
     }
 
     @GetMapping("/responsive_table")
