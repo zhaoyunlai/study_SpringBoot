5d43dc249fee6b0957d0681744a84db3640f61f8
zhaoyunlai <zylai0712@163.com>
2022-08-13 23:18:05 +0800

    SpringBoot jasypt加密

--- boot-04-web-admin/src/main/java/com/atguigu/boot/Boot04WebAdminApplication.java
+++ boot-04-web-admin/src/main/java/com/atguigu/boot/Boot04WebAdminApplication.java
@@ -4,6 +4,8 @@ import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.ServletComponentScan;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.core.env.Environment;
 
 //扫描servlet
 @ServletComponentScan(basePackages = "com.atguigu.boot.servlet")
@@ -13,7 +15,16 @@ import org.springframework.boot.web.servlet.ServletComponentScan;
 public class Boot04WebAdminApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(Boot04WebAdminApplication.class, args);
+        ConfigurableApplicationContext run = SpringApplication.run(Boot04WebAdminApplication.class, args);
+        Environment environment = run.getBean(Environment.class);
+        //查看一下加密的密匙，测试用
+        System.out.println(environment.getProperty("jasypt.encryptor.password"));
+
+        //在程序启动时，jasypt会解密ENC()中的内容
+        String mysqlPassword = environment.getProperty("spring.datasource.password");
+        String mysqlUsername = environment.getProperty("spring.datasource.username");
+        System.out.println("测试数据加密，解密之后的mysql账号密码为：");
+        System.out.println("mysqlPassword:"+mysqlPassword+"\nmysqlUsername:"+mysqlUsername);
     }
 
 }
---
+++ boot-04-web-admin/src/test/java/com/atguigu/boot/EncryptTest.java
@@ -0,0 +1,43 @@
+package com.atguigu.boot;
+
+import org.jasypt.encryption.StringEncryptor;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
+import org.springframework.core.env.Environment;
+
+/**
+ * @Author: Zhao YunLai
+ * @Date: 2022/08/13/20:11
+ * @Description:
+ */
+@SpringBootTest
+public class EncryptTest {
+
+    //加密的工具
+    @Autowired
+    private StringEncryptor stringEncryptor;
+
+    @Autowired
+    private ApplicationContext applicationContext;
+
+    @Test
+    public void getEncryptedPwd(){
+//        Environment environment = applicationContext.getBean(Environment.class);
+//        //获取配置文件中的明文密码
+//        String mysqlPassword = environment.getProperty("spring.datasource.password");
+//        String redisPassword = environment.getProperty("spring.redis.password");
+//        System.out.println("明文信息为如下：");
+//        System.out.println("mysqlPassword:"+mysqlPassword+"\nredisPassword:"+redisPassword);
+
+        System.out.println("========加密之后的信息如下===============");
+        System.out.println("mysqlPassword:"+encrypt("root"));
+        System.out.println("redisPassword:"+encrypt("123321"));
+        System.out.println("mysqlUsername:"+encrypt("root"));
+    }
+
+    public String encrypt(String str){
+        return stringEncryptor.encrypt(str);
+    }
+}
